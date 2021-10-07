e57cb4cf3449d96b7c222ca7f53cb4b7bdfb04b7
Esteban Morales <esteban.andres.morales@mi.unc.edu.ar>
2021-10-07 03:13:39 -0300

    minimo funcional

---
+++ src/main/java/com/steve/sbootandcrnkbasic/SbootandcrnkbasicApplication.java
@@ -0,0 +1,15 @@
+package com.steve.sbootandcrnkbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(CorsConfig.class)
+public class SbootandcrnkbasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootandcrnkbasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steve/sbootandcrnkbasic/SbootandcrnkbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.sbootandcrnkbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootandcrnkbasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
