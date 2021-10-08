80ab38cf717de560b4d27d728cc90a6523f2e182
Esteban Morales <esteban.andres.morales@mi.unc.edu.ar>
2021-10-07 22:24:59 -0300

    with JPA added on the build.gradle fails to run.

--- src/main/java/com/steve/sbootandcrnkbasic/SbootandcrnkbasicApplication.java
+++ src/main/java/com/steve/sbootandcrnkbasic/SbootandcrnkbasicApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(CorsConfig.class)
+
 public class SbootandcrnkbasicApplication {
 
     public static void main(String[] args) {
